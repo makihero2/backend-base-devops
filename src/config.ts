e3dd93af037819e7ebd2bfdb874b4b98274090bc
juanHernandez <jhernandezd4d@gmail.com>
2024-09-11 18:00:45 -0300

    modificacion pruebas

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const configuration = {
-    username: process.env.USERNAME ?? "Juan Hernandez D.",
+    username: process.env.USERNAME ?? "JHD",
     port: process.env.PORT ?? 3001,
     apiKey: process.env.API_KEY ?? "Desconocida"
 }
